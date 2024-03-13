a7056699ac346ea076e87374b74baa97ce0fe966
Aaron Diggdon <aarondiggdon@gmail.com>
2024-03-13 22:09:02 +0000

    Restarting application

---
+++ src/App.js
@@ -0,0 +1,46 @@
+// Put your code below this line.
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+
+
+const App = () => {
+
+
+
+  
+  
+  
+
+
+
+
+    
+
+
+// async function getTodo() {
+//   try {
+//     const restOperation = get({ 
+//       apiName: 'z2nfgfuf2e',
+//       path: '/pf'
+//     });
+//     const response = await restOperation.response;
+//     console.log('GET call succeeded: ', response);
+//   } catch (e) {
+//     console.log('GET call failed: ', e.response);
+//   }
+// }
+   
+
+
+
+  return (
+    <div className="App">
+     
+     
+    </div>
+  );
+};
+
+export default App;
